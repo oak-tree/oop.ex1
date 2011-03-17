372ca9f1294e617d1b8d583a5c66bd647cae0287
oak <k.o.alon@gmail.com>
2011-03-17 13:29:00 +0200

    add fuction for read file into butffer

---
+++ src/Parser/fileFunctions.java
@@ -0,0 +1,21 @@
+package Parser;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class fileFunctions {
+
+	  public static String readFileAsString(String filePath) throws java.io.IOException{
+		    byte[] buffer = new byte[(int) new File(filePath).length()];
+		    BufferedInputStream f = null;
+		    try {
+		        f = new BufferedInputStream(new FileInputStream(filePath));
+		        f.read(buffer);
+		    } finally {
+		        if (f != null) try { f.close(); } catch (IOException ignored) { }
+		    }
+		    return new String(buffer);
+		}
+}
