81da89651ac67ef0ef99eecec0251f476993401a
oak <k.o.alon@gmail.com>
2011-03-18 18:01:36 +0200

    size filter. will be father of Greater and Smaller

---
+++ src/filters/SizeFilter.java
@@ -0,0 +1,14 @@
+package filters;
+
+public class SizeFilter   extends FilterFactory implements FilterInterface {
+
+public SizeFilter() {
+
+}
+
+@Override
+public void runCommand() {
+	// TODO Auto-generated method stub
+	
+}
+}
