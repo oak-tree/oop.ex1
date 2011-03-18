73207c167575a20692c4af318258689e5fe41a6e
oak <k.o.alon@gmail.com>
2011-03-18 18:02:08 +0200

    Date Filter, file wasnt here before. so i add this one now

---
+++ src/filters/DateFilter.java
@@ -0,0 +1,14 @@
+package filters;
+
+public class DateFilter extends FilterFactory implements FilterInterface {
+
+	public DateFilter(){
+		//do something 
+	}
+	@Override
+	public void runCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
