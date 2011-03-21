8ded1998c99ec92d44dc6de19f1dce17fb8b4880
oak <k.o.alon@gmail.com>
2011-03-21 15:55:01 +0200

    try to merge

--- src/filters/OrOperator.java
+++ src/filters/OrOperator.java
@@ -1,7 +1,16 @@
 package filters;
 
+import java.util.List;
+
+import commands.Command;
+
 public class OrOperator extends BinaryOperatorFilter {
 
+
+	public OrOperator(List<filter> filterList) {
+		super(filterList);
+		// TODO Auto-generated constructor stub
+	}
 
 	protected boolean binaryOperator(boolean b1, boolean b2) {
 		// TODO Auto-generated method stub
