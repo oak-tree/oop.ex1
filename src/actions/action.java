5a11c683c6c538ed958d9b8f8765dd004a328f64
unknown <ami@.(none)>
2011-03-21 15:43:17 +0200

    delete action.java

--- src/actions/action.java
+++ src/actions/action.java
@@ -1,11 +1,15 @@
 package actions;
 
+import java.io.File;
+
 import commands.Command;
 
-public abstract class action implements Command {
+public abstract class Action implements Command {
 	public String[] runCommand(String[] filenames) {
 		return null;
 		// TODO Auto-generated method stub
 
 	}
+	
+	abstract protected void performAction(File f);
 }
