4ba04e0383c027030db8d688be508588fa2cb1d0
oak <k.o.alon@gmail.com>
2011-03-22 18:08:53 +0200

    tester

--- tester/runtests.sh
+++ tester/runtests.sh
@@ -6,12 +6,12 @@
 FILES=*.in
 MAX_NO=0
 
-if [ $# -ne 1 ]
-then
-   echo "use: %thisfile.sh [filename with no extension] "
-   exit 1
-fi
-echo "tester for intro, compring output vs school solution"
+#if [ $# -ne 2 ]
+#then
+#   echo "use: %thisfile.sh [filename with no extension] "
+#   exit 1
+#fi
+echo "tester for oop, compring output vs school solution"
 echo "press enter to contiune"
 read
 echo "compling all *.java in folder"
@@ -19,7 +19,7 @@ javac *.java
 #clear
 echo "press enter to contiune"
 read
-echo "testing $1."
+
 echo "all files in the form of *.in will be used as input."
 echo "note: if a diffrence has been found. the loop will break. and not all the input files will be checked"
 echo "press enter to contiune"
@@ -33,7 +33,8 @@ WORKPATH="~/tests"
 			# echo "input:"
 			# cat $f 
 
-			java $WORKPATH  $f > myout1
+			java MyFileScript
+$WORKPATH  $f > myout1
 			~oop/for_students/school_solutions/ex1/ex1SchoolSolution
 $WORKPATH  $f sout1
 			echo "*****diff starts"
